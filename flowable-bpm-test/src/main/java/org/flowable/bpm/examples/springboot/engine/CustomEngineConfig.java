c0a58c24f6a150c3bc481ecb1a25556420189144
Alex Chabatar <alex.chabatar@vodeno.com>
2025-06-05 11:28:21 +0200

    switch to SingleResourceAutoDeploymentStrategy

--- flowable-bpm-test/src/main/java/org/flowable/bpm/examples/springboot/engine/CustomEngineConfig.java
+++ flowable-bpm-test/src/main/java/org/flowable/bpm/examples/springboot/engine/CustomEngineConfig.java
@@ -1,10 +1,24 @@
 package org.flowable.bpm.examples.springboot.engine;
 
+import org.flowable.spring.SpringProcessEngineConfiguration;
+import org.flowable.spring.boot.EngineConfigurationConfigurer;
+import org.flowable.spring.configurator.SingleResourceAutoDeploymentStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CustomEngineConfig {
+
+  /**
+   * By default, Flowable will use a single deployment for all resources.
+   * @see <a href="https://forum.flowable.org/t/updating-single-bpmn-creates-new-process-definition-versions-for-all/8215">Flowable Forum</a>
+   *
+   * @return
+   */
+  @Bean
+  public EngineConfigurationConfigurer<SpringProcessEngineConfiguration> defaultEngineConfigurer() {
+    return engineConfiguration -> engineConfiguration.setDeploymentMode(SingleResourceAutoDeploymentStrategy.DEPLOYMENT_MODE);
+  }
 
   @Bean
   public CustomEngineConfigurer customEngineConfigurer() {
